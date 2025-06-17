003a3c199a92700c22310d758f91b8ecf353757a
unknown <sifontes.cabs@gmail.com>
2025-06-17 01:40:30 -0300

    Agregando componente productos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import CarouselC from "../components/carousel/CarouselC";
 import ServiciosC from "../components/servicios/ServiciosC";
+import ProductosC from "../components/productos/ProductosC"; 
 import "../App.css";
 
 const Home = () => {
   return (
-    <>      
+    <>
       <CarouselC />
       <ServiciosC />
+      <ProductosC />
     </>
   );
 };
