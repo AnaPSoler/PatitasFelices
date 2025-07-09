1658f068c053af9a485648516d6c83d73e4632ae
Ana Paula Soler <anapaulasoler711@gmail.com>
2025-07-09 09:07:15 -0300

    agregamos archivo vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@rollup/rollup-linux-x64-gnu']
-    }
-  }
 });
-
